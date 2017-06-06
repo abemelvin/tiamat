4e956f4e7f377d822809905b13ad6323dc54d944
abemelvin <abe.melvin@gmail.com>
2017-06-06 13:59:20 -0400

    web app script fixes

--- terraform/file_provision/scripts/webapp.sh
+++ terraform/file_provision/scripts/webapp.sh
@@ -9,10 +9,8 @@ sudo pip install django==1.10
 sudo apt-get install -y apache2
 sudo apt-get install -y libapache2-mod-wsgi
 
-cd ~
-git clone https://github.com/abemelvin/tiamat/
-mv tiamat/web-server web-server
-cd ~
+sudo git clone https://github.com/abemelvin/tiamat/
+sudo mv tiamat/web-server web-server
 sudo chgrp -R www-data web-server
 sudo chmod -R g+w web-server
 sudo apache2ctl restart
