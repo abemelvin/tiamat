7ad465d99747a224746e3e3d5a0a0ae7654c616d
abemelvin <abe.melvin@gmail.com>
2017-06-07 11:41:46 -0400

    created box for ldap

--- terraform/configuration.tf
+++ terraform/configuration.tf
@@ -52,12 +52,30 @@ resource "aws_route53_record" "contractor" {
   depends_on = ["aws_instance.contractor", "aws_route53_zone.terraform"]
 }
 
-resource "aws_route53_record" "mail" {
+resource "aws_route53_record" "ldap" {
+  zone_id = "${aws_route53_zone.terraform.zone_id}"
+  name = "ldap.fazio.com"
+  type = "A"
+  ttl = "300"
+  records = ["${aws_instance.ldap.private_ip}"]
+  depends_on = ["aws_instance.ldap", "aws_route53_zone.terraform"]
+}
+
+resource "aws_route53_record" "mail_A" {
   zone_id = "${aws_route53_zone.terraform.zone_id}"
   name = "mail.fazio.com"
   type = "A"
   ttl = "300"
   records = ["${aws_instance.mail.private_ip}"]
+  depends_on = ["aws_instance.mail", "aws_route53_zone.terraform"]
+}
+
+resource "aws_route53_record" "mail_MX" {
+  zone_id = "${aws_route53_zone.terraform.zone_id}"
+  name = "fazio.com"
+  type = "MX"
+  ttl = "300"
+  records = ["50 mail.fazio.com"]
   depends_on = ["aws_instance.mail", "aws_route53_zone.terraform"]
 }
 
@@ -209,7 +227,26 @@ resource "aws_instance" "webapp" {
   depends_on = ["aws_route_table.terraform", "aws_security_group.terraform", "aws_subnet.terraform"]
 
   connection {
-    host = "${aws_instance.mail.public_ip}"
+    host = "${aws_instance.webapp.public_ip}"
+    type = "ssh"
+    user = "ubuntu"
+    private_key = "${file("key")}"
+    agent = false
+  }
+}
+
+resource "aws_instance" "ldap" {
+  ami = "ami-f4cc1de2"
+  instance_type = "t2.medium"
+  security_groups = ["${aws_security_group.terraform.id}"]
+  key_name = "key"
+  subnet_id = "${aws_subnet.terraform.id}"
+  associate_public_ip_address = true
+  private_ip = "10.0.0.16"
+  depends_on = ["aws_route_table.terraform", "aws_security_group.terraform", "aws_subnet.terraform"]
+
+  connection {
+    host = "${aws_instance.ldap.public_ip}"
     type = "ssh"
     user = "ubuntu"
     private_key = "${file("key")}"
@@ -235,4 +272,8 @@ output "mail ip" {
 
 output "webapp ip" {
   value = "${aws_instance.webapp.public_ip}"
+}
+
+output "ldap ip" {
+  value = "${aws_instance.ldap.public_ip}"
 }
