7a9b01e2681cca74ca20f2189d33a7a626b18045
VDCN12593 <winson.wu51@gmail.com>
2017-06-15 18:33:55 -0400

    add elk function

--- terraform/cliff-shell/Tiamat.py
+++ terraform/cliff-shell/Tiamat.py
@@ -18,6 +18,7 @@ is_deployed = False
 ansible_ip = ""
 elk_ip = ""
 elk_logs_path = ""
+os_platform = ""
 
 
 class Tiamat(App):
@@ -33,12 +34,22 @@ class Tiamat(App):
             'deploy': Deploy,
             'destroy': Destroy,
             'ansible': Ansible,
-            'get-elk-files': Elk
+            'get-elk-files': ElkFiles,
+            'elk': Elk
         }
         for k, v in commands.iteritems():
             self.command_manager.add_command(k, v)
 
         self.command_manager.add_command('complete', cliff.complete.CompleteCommand)
+
+        # os platform check
+        global os_platform
+        if sys.platform.startswith('linux'):
+            os_platform = "Linux"
+        elif sys.platform.startswith('darwin'):
+            os_platform = "OS X"
+        elif sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
+            os_platform = "Windows"
 
         # start dependencies check
         if "AWS_ACCESS_KEY_ID" not in os.environ:
@@ -62,7 +73,7 @@ class Tiamat(App):
                 local_path = raw_input("Please input full local file directory --> ")
                 local_file_path = local_path + '/terraform.zip'
 
-                if sys.platform.startswith('linux'):
+                if os_platform == "Linux":
                     if is_64bits:
                         url = "https://releases.hashicorp.com/terraform/0.9.8/terraform_0.9.8_" + \
                             "linux_amd64.zip?_ga=2.142026481.2126347023.1497377866-658368258.1496936210"
@@ -74,7 +85,7 @@ class Tiamat(App):
                     unzip_call = "unzip " + local_file_path + " -d " + local_path
                     subprocess.check_call(unzip_call, shell=True)
 
-                elif sys.platform.startswith('darwin'):
+                elif os_platform == "OS X":
                     url = "https://releases.hashicorp.com/terraform/0.9.8/terraform_0.9.8" + \
                         "_darwin_amd64.zip?_ga=2.76410710.2126347023.1497377866-658368258.1496936210"
                     curl_call = "curl " + url + " -o " + local_file_path
@@ -82,7 +93,7 @@ class Tiamat(App):
                     unzip_call = "unzip " + local_file_path + "-d " + local_path
                     subprocess.check_call(unzip_call, shell=True)  # can use -d to assign exp dir
 
-                elif sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
+                elif os_platform == "Windows":
                     if is_64bits:
                         url = "https://releases.hashicorp.com/terraform/0.9.8/terraform_0.9.8_" + \
                             "windows_amd64.zip?_ga=2.176148193.2126347023.1497377866-658368258.1496936210"
@@ -195,12 +206,12 @@ class Ansible(Command):
         subprocess.check_call(ssh_call, shell=True)
 
 
-class Elk(Command):
+class ElkFiles(Command):
     """Copy log files from ELK server to local folder"""
     log = logging.getLogger(__name__)
 
     def get_parser(self, prog_name):
-        parser = super(Elk, self).get_parser(prog_name)
+        parser = super(ElkFiles, self).get_parser(prog_name)
         parser.add_argument('local_path')
         return parser
 
@@ -208,12 +219,34 @@ class Elk(Command):
         self.log.debug('debugging')
         global elk_ip
         global elk_logs_path
-        if not ansible_ip:
+        if not elk_ip:
             self.app.stdout.write("Error: ELK IP unavailable.\n")
             return
 
         scp_call = "scp -i key -r ubuntu@" + elk_ip + ':' + elk_logs_path + ' ' + parsed_args.local_path
         subprocess.check_call(scp_call, shell=True)
+
+
+class Elk(Command):
+    """open the Elk Dashboard in user's default browser"""
+    log = logging.getLogger(__name__)
+
+    def take_action(self, parsed_args):
+        self.log.debug('debugging')
+        global elk_ip
+        if not elk_ip:
+            self.app.stdout.write("Error: ELK IP unavailable.\n")
+            return
+
+        global os_platform
+        if os_platform == "Linux":
+            browser_call = "xdg-open " + 'http://' + elk_ip
+        elif os_platform == "OS X":
+            browser_call = "open " + 'http://' + elk_ip
+        elif os_platform == "Windows":
+            browser_call = "explorer " + 'http://' + elk_ip
+
+        subprocess.check_call(browser_call, shell=True)
 
 
 if __name__ == '__main__':
